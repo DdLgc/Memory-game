34ab9d9e00a17a8fe14b0f87f79d5d8b3c569e30
David Lgc <legouellecd@gmail.com>
2026-07-04 19:04:59 +0200

    feat: duplicateArray function + 16 cards add

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-// const cards = [
-//   "https://picsum.photos/id/10/100/100",
-//   "https://picsum.photos/id/11/100/100",
-//   "https://picsum.photos/id/12/100/100",
-//   "https://picsum.photos/id/13/100/100",
-//   "https://picsum.photos/id/14/100/100",
-//   "https://picsum.photos/id/15/100/100",
-//   "https://picsum.photos/id/16/100/100",
-//   "https://picsum.photos/id/17/100/100",
-// ];
+const cards = [
+  "https://picsum.photos/id/10/100/100",
+  "https://picsum.photos/id/11/100/100",
+  "https://picsum.photos/id/12/100/100",
+  "https://picsum.photos/id/13/100/100",
+  "https://picsum.photos/id/14/100/100",
+  "https://picsum.photos/id/15/100/100",
+  "https://picsum.photos/id/16/100/100",
+  "https://picsum.photos/id/17/100/100",
+];
 
 const gameBoard = document.getElementById("game-board");
 gameBoard.appendChild(createCard("https://picsum.photos/id/236/100/100"));
@@ -24,15 +24,15 @@ function createCard(CardUrl) {
   return card;
 }
 
-// function duplicateArray(arraySimple) {
-//   let arrayDouble = [];
-//   arrayDouble.push(...arraySimple);
-//   arrayDouble.push(...arraySimple);
-//   return arrayDouble;
-// }
+function duplicateArray(arraySimple) {
+  let arrayDouble = [];
+  arrayDouble.push(...arraySimple);
+  arrayDouble.push(...arraySimple);
+  return arrayDouble;
+}
 
-// const allCards = duplicateArray(cards);
-// allCards.forEach((card) => {
-//   const cardHtml = createCard(card);
-//   gameBoard.appendChild(cardHtml);
-// });
+const allCards = duplicateArray(cards);
+allCards.forEach((card) => {
+  const cardHtml = createCard(card);
+  gameBoard.appendChild(cardHtml);
+});
