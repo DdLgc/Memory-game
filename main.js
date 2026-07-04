a9b7b1cf1ae4374b3b7978c2c67095be93a85739
David Lgc <legouellecd@gmail.com>
2026-07-04 19:01:50 +0200

    feat: initialize game board and add createCard function

--- main.js
+++ main.js
@@ -1,14 +1,38 @@
+// const cards = [
+//   "https://picsum.photos/id/10/100/100",
+//   "https://picsum.photos/id/11/100/100",
+//   "https://picsum.photos/id/12/100/100",
+//   "https://picsum.photos/id/13/100/100",
+//   "https://picsum.photos/id/14/100/100",
+//   "https://picsum.photos/id/15/100/100",
+//   "https://picsum.photos/id/16/100/100",
+//   "https://picsum.photos/id/17/100/100",
+// ];
+
+const gameBoard = document.getElementById("game-board");
+gameBoard.appendChild(createCard("https://picsum.photos/id/236/100/100"));
+
 function createCard(CardUrl) {
-    const card = document.createElement('div');
-    card.classList.add('card');
-    card.dataset.value = CardUrl;
-  
-    const cardContent = document.createElement('img');
-    cardContent.classList.add('card-content');
-    cardContent.src = `${CardUrl}`;
-    card.appendChild(cardContent);
-    return card;
-  }
-//Code pour tester notre fonction
-const gameBoard = document.getElementById('game-board');
-gameBoard.appendChild(createCard('https://picsum.photos/id/10/100/100'));
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.dataset.value = CardUrl;
+
+  const cardContent = document.createElement("img");
+  cardContent.classList.add("card-content");
+  cardContent.src = `${CardUrl}`;
+  card.appendChild(cardContent);
+  return card;
+}
+
+// function duplicateArray(arraySimple) {
+//   let arrayDouble = [];
+//   arrayDouble.push(...arraySimple);
+//   arrayDouble.push(...arraySimple);
+//   return arrayDouble;
+// }
+
+// const allCards = duplicateArray(cards);
+// allCards.forEach((card) => {
+//   const cardHtml = createCard(card);
+//   gameBoard.appendChild(cardHtml);
+// });
