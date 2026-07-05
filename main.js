8e110cdb35f2938a633356e5d755224a806fbb7e
David Lgc <legouellecd@gmail.com>
2026-07-05 10:58:44 +0200

    feat: add game timer

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const cards = [
 ];
 
 const gameBoard = document.getElementById("game-board");
-
+let selectedCards = [];
 
 function createCard(CardUrl) {
   const card = document.createElement("div");
@@ -20,7 +20,10 @@ function createCard(CardUrl) {
   const cardContent = document.createElement("img");
   cardContent.classList.add("card-content");
   cardContent.src = `${CardUrl}`;
+
   card.appendChild(cardContent);
+
+  card.addEventListener("click", onCardClick);
   return card;
 }
 
@@ -31,13 +34,42 @@ function duplicateArray(arraySimple) {
   return arrayDouble;
 }
 
+function shuffleArray(arrayToshuffle) {
+  const arrayShuffled = arrayToshuffle.sort(() => 0.5 - Math.random());
+  return arrayShuffled;
+}
+
+function onCardClick(e) {
+  const card = e.target.parentElement;
+  card.classList.add("flip");
+
+  selectedCards.push(card);
+  if (selectedCards.length == 2) {
+    setTimeout(() => {
+      if (selectedCards[0].dataset.value == selectedCards[1].dataset.value) {
+        selectedCards[0].classList.add("matched");
+        selectedCards[1].classList.add("matched");
+        selectedCards[0].removeEventListener("click", onCardClick);
+        selectedCards[1].removeEventListener("click", onCardClick);
+
+        // const allCardNotFinded = document.querySelectorAll(
+        //   ".card:not(.matched)",
+        // );
+        // if (allCardNotFinded.length == 0) {
+        //   alert("Bravo, vous avez gagné");
+        // }
+      } else {
+        selectedCards[0].classList.remove("flip");
+        selectedCards[1].classList.remove("flip");
+      }
+      selectedCards = [];
+    }, 1000);
+  }
+}
+
 let allCards = duplicateArray(cards);
+allCards = shuffleArray(allCards);
 allCards.forEach((card) => {
   const cardHtml = createCard(card);
   gameBoard.appendChild(cardHtml);
 });
-
-function shuffleArray(arrayToshuffle){
-    const arrayShuffled = arrayToshuffle.sort(() => 0.5 - Math.random());
-    return arrayShuffled;
-}
